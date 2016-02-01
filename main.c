bdbce87edb4466526af5a9d84e987fe4da56f8f5
rungokarol <karolrungo@gmail.com>
2016-02-01 19:28:25 +0100

    empty loop in main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: 1 lut 2016
+ *      Author: Karol
+ */
+
+#ifndef MAIN_C_
+#define MAIN_C_
+
+int main(void)
+{
+	while(1)
+	{
+
+	}
+return 0;
+}
+
+#endif /* MAIN_C_ */
