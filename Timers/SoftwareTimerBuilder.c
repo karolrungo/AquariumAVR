29a499f489f2fcfae3c948c6e333b8e4adc78169
rungokarol <karolrungo@gmail.com>
2016-06-01 18:23:45 +0200

    timer builder refactored

--- Timers/SoftwareTimerBuilder.c
+++ Timers/SoftwareTimerBuilder.c
@@ -9,22 +9,26 @@
 
 #include <stdbool.h>
 
+static Timer buildCommonPart(Miliseconds p_miliseconds, Callback p_callback);
+
 Timer buildOneShotTimer(Miliseconds p_miliseconds, Callback p_callback)
 {
-	Timer l_timer;
-	l_timer.isRunning = true;
+	Timer l_timer = buildCommonPart(p_miliseconds, p_callback);
 	l_timer.isOneShot = true;
-	l_timer.latency = p_miliseconds;
-	l_timer.timeLeft = l_timer.latency;
-	l_timer.callback = p_callback;
 	return l_timer;
 }
 
 Timer buildPeriodicalTimer(Miliseconds p_miliseconds, Callback p_callback)
 {
+	Timer l_timer = buildCommonPart(p_miliseconds, p_callback);
+	l_timer.isOneShot = false;
+	return l_timer;
+}
+
+Timer buildCommonPart(Miliseconds p_miliseconds, Callback p_callback)
+{
 	Timer l_timer;
 	l_timer.isRunning = true;
-	l_timer.isOneShot = false;
 	l_timer.latency = p_miliseconds;
 	l_timer.timeLeft = l_timer.latency;
 	l_timer.callback = p_callback;
